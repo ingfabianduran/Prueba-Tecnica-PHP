e9cfe7837c61312abd84f485cec6daa8060853d6
Fabian Duran <ingfabianduran@gmail.com>
2022-01-19 10:45:17 -0500

    Titulos sobre las consultas adicionales

--- consultas_adicionales.sql
+++ consultas_adicionales.sql
@@ -1,3 +1,5 @@
+Productos con mas Stock:
 SELECT nombre, stock FROM `productos` ORDER BY stock DESC LIMIT 1;
 SELECT nombre, stock FROM productos WHERE stock = (SELECT MAX(stock) FROM productos);
+Producto mas Vendido:
 SELECT productos.nombre as nombre_producto, COUNT(ventas.producto_id) as total_compras FROM ventas JOIN productos ON ventas.producto_id = productos.id GROUP BY ventas.producto_id ORDER BY total_compras DESC LIMIT 1;
